721935e11227179a4a045091988d8fa7c53c8700
Samparkyn <samparkyn101@gmail.com>
2016-10-18 19:45:56 +0100

    Add methods to get dates, rates by the dates & currencies + tests

--- specs/converter_spec.rb
+++ specs/converter_spec.rb
@@ -12,4 +12,15 @@ class TestConverter < MiniTest::Test
     assert_equal("2016-10-14", @converter.get_dates[0])
     assert_equal(65, @converter.get_dates.count)
   end
+  
+  def test_get_rates_by_date
+    date = "2016-10-14"
+    assert_equal(date, @converter.get_rates_by_date(date)['date'])
+    assert_equal({"USD" => "1.1002"}, @converter.get_rates_by_date(date)['rates'][0])
+  end
+  
+  def test_get_currencies
+    assert_equal("USD", @converter.get_currencies[0])
+    assert_equal("JPY", @converter.get_currencies[1])
+  end
 end
